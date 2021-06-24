1d0fe99be99148c8d1ff24b5fbb1cdc3c81bd609
danydf1 <reitmatch@hotmail.com>
2021-06-24 00:11:47 -0300

    arma tu pc todavia no terminado

--- TPC_Fernandez-Herrera/armaTuPc.aspx
+++ TPC_Fernandez-Herrera/armaTuPc.aspx
@@ -5,5 +5,26 @@
     <!-- arma tu pc se muestra una tanda de card que se van a separar por cada categoria 
         el orden tiene que ser 1-motherboard 2-procesador 3-coolers/ventiladores 4-ram 5-fuentes 6-placa de video 7-gabinete 8-pantalla -->
 
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
 
+            <%foreach (Dominio.Componente item in listaComponente)
+                {%>
+
+            <div class="col">
+
+                <div class="card border-dark mb-3" style="width: 18rem; margin-top:20px">
+                    <img src="<%= item.ImagenUrl %>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><%= item.Nombre %></h5>
+                        <p class="card-text"><%= item.Descripcion %></p>
+                        <p class="card-text"><span class="badge bg-secondary" style="font-size: 20px"><%= item.Precio %></span></p>
+                        <asp:button ID="btnAgregar" class="btn btn-secondary" runat="server" OnClick="btnAgregar_Click" Text="Agregar" />
+                    </div>
+                </div>
+            </div>
+            <%} %>
+        </div>
+
+    </div>
 </asp:Content>
--- TPC_Fernandez-Herrera/armaTuPc.aspx.cs
+++ TPC_Fernandez-Herrera/armaTuPc.aspx.cs
@@ -4,12 +4,58 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace TPC_Fernandez_Herrera
 {
     public partial class armaTuPc : System.Web.UI.Page
     {
+        public List<Componente> listaComponente ;
+        List<Componente> listaArmado;
         protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listaArmado == null)
+                {
+                    cargarCatalogo();
+                    listaComponente = (List<Componente>)Session["ListaComponente"];
+                    Session.Add("armadoPc", listaArmado);
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public void cargarCatalogo()
+        {
+            
+            List<Componente> listaComponentes = (List<Componente>)Session["ListarComponentes"];
+            
+            if (listaArmado == null)
+            {
+                listaComponente = listaComponentes.FindAll(x => x.categoria.Nombre.Contains("Procesadores"));
+                Session.Add("listaComponente", listaComponente);
+                
+            }
+            else if(listaArmado.Count == 1){
+                listaComponente = listaComponentes.FindAll(x => x.categoria.Nombre.ToUpper().Contains("cartucho"));
+                Session.Add("listaComponente", listaComponente);
+            }
+
+        }
+
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
         {
 
         }
