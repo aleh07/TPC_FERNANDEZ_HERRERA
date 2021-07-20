a72456ace66928bfb662dc77074e2fd8a6896f9e
danydf1 <reitmatch@hotmail.com>
2021-07-20 05:21:31 -0300

    ya se lista el alta del componente a la tabla

--- TPC_Fernandez-Herrera/AltaComponente.aspx.cs
+++ TPC_Fernandez-Herrera/AltaComponente.aspx.cs
@@ -47,15 +47,37 @@ namespace TPC_Fernandez_Herrera
             }
             if (id != null)
             {
-                Componente modificar = listaComponente.Find(x => x.ID.ToString() == id);
-                TxtNombre.Text = modificar.Nombre;
-                TxtDescripcion.Text = modificar.Descripcion;
-                TxtPrecio.Text = modificar.Precio.ToString();
-                TxtCant.Text = modificar.Cantidad.ToString();
-                TxtImagenUrl.Text = modificar.ImagenUrl;
-                ddlCategoria.SelectedIndex = modificar.categoria.Id -1;
-                ddlMarca.SelectedIndex = modificar.marca.Id -1;
+                CargarForm(id);
+                
             }
+        }
+
+        public void CargarForm(string id)
+        {
+            Componente modificar = listaComponente.Find(x => x.ID.ToString() == id);
+
+            TxtNombre.Text = modificar.Nombre;
+            TxtDescripcion.Text = modificar.Descripcion;
+            TxtPrecio.Text = modificar.Precio.ToString();
+            TxtCant.Text = modificar.Cantidad.ToString();
+            TxtImagenUrl.Text = modificar.ImagenUrl;
+            ddlCategoria.SelectedIndex = modificar.categoria.Id - 1;
+            ddlMarca.SelectedIndex = modificar.marca.Id - 1;
+
+        }
+        public void GuardarForm(Componente aux)
+        {
+            List<Marca> marcas = (List<Marca>)Session["listaMarcas"];
+            List<Categoria> categorias = (List<Categoria>)Session["listaCat"];
+            aux.Nombre = TxtNombre.Text;
+            aux.Descripcion = TxtDescripcion.Text;
+            aux.ImagenUrl = TxtImagenUrl.Text;
+            aux.Cantidad = Convert.ToInt32(TxtCant.Text);
+            aux.Estado = true;
+            aux.marca = marcas.Find(x => x.Id == int.Parse(ddlMarca.SelectedValue));
+            aux.categoria = categorias.Find(x => x.Id == Convert.ToInt32(ddlCategoria.SelectedItem.Value));
+            if (TxtPrecio.Text == "") aux.Precio = 0;
+            aux.Precio = decimal.Parse(TxtPrecio.Text);
         }
 
         bool ValidarVacios()
@@ -96,11 +118,15 @@ namespace TPC_Fernandez_Herrera
             List<Categoria> categorias = (List<Categoria>)Session["listaCat"];
             string id = Request.QueryString["ID"];
             
+           
+
             if (ValidarVacios()==false)
             {
                 if (id==null)
                 {
-                   //el problema es que no refresca ver el video de mxi, kDA CARGADO CON LA VARIABLE MODIFICAR,PD ESTO NO VA ACA
+                    List<Componente> ListaAux = new List<Componente>();
+                    ListaAux = (List<Componente>)Session["ListarComponentes"];
+                    //el problema es que no refresca ver el video de mxi, kDA CARGADO CON LA VARIABLE MODIFICAR,PD ESTO NO VA ACA
                     aux.Nombre = TxtNombre.Text;
                     aux.Descripcion = TxtDescripcion.Text;
                     aux.ImagenUrl = TxtImagenUrl.Text;
@@ -112,21 +138,35 @@ namespace TPC_Fernandez_Herrera
                     aux.Precio = decimal.Parse(TxtPrecio.Text);
 
                     negocio.agregar(aux);
+
+                    ListaAux.Add(aux);
+                    Session["ListarComponentes"] = ListaAux;
                     ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Alta Registrada');window.location ='GestionStock.aspx';", true);
+                                
                 }
                 else {
-                    Componente modificar = listaComponente.Find(x => x.ID.ToString() == id);
 
-                    modificar.Nombre = TxtNombre.Text;
-                    modificar.Descripcion = TxtDescripcion.Text;
-                    modificar.ImagenUrl = TxtImagenUrl.Text;
-                    modificar.Cantidad = int.Parse(TxtCant.Text);
-                    modificar.Estado = true;
-                    modificar.marca = marcas.Find(x => x.Id == int.Parse(ddlMarca.SelectedValue));
-                    modificar.categoria = categorias.Find(x => x.Id == Convert.ToInt32(ddlCategoria.SelectedItem.Value));
-                    modificar.Precio = decimal.Parse(TxtPrecio.Text);
                     
-                    negocio.modificar(modificar);
+                    List<Componente> ListaAux = new List<Componente>();
+                    ListaAux = (List<Componente>)Session["ListarComponentes"];
+
+                    //aux.Nombre = TxtNombre.Text;
+                    //aux.Descripcion = TxtDescripcion.Text;
+                    //aux.ImagenUrl = TxtImagenUrl.Text;
+                    //aux.Cantidad = Convert.ToInt32(TxtCant.Text);
+                    //aux.Estado = true;
+                    //aux.marca = marcas.Find(x => x.Id == int.Parse(ddlMarca.SelectedValue));
+                    //aux.categoria = categorias.Find(x => x.Id == Convert.ToInt32(ddlCategoria.SelectedItem.Value));
+                    //if (TxtPrecio.Text == "") aux.Precio = 0;
+                    //aux.Precio = decimal.Parse(TxtPrecio.Text);
+                    GuardarForm(aux);
+                   
+                    negocio.modificar(aux);
+                   
+                    ListaAux.RemoveAll(item => item.ID == int.Parse(Request.QueryString["ID"]));
+                    ListaAux.Add(aux);
+                    Session["ListarComponentes"] = ListaAux;
+
                     ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Registro Modificado');window.location ='GestionStock.aspx';", true);
                 }
             }
